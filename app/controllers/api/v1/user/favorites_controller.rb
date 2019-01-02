efff5d9623af3daed4dabe8563f0c944edfcdc4c
normanrs <normanrs@live.com>
2019-01-02 14:15:11 -0700

    working thru story 3

--- app/controllers/api/v1/user/favorites_controller.rb
+++ app/controllers/api/v1/user/favorites_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::User::FavoritesController < ApiBaseController
   def index
-    if current_user.api_key.value == params["api_key"]
+    if current_user.api_key.value == search_params["api_key"]
       user_asteroids = current_user.favorites.map do |fav|
         fav
       end
@@ -8,6 +8,22 @@ class Api::V1::User::FavoritesController < ApiBaseController
     else
       render json: {status: "error", code: 403, message: "Trying to use someone else's API key is very naughty. Bad user! Bad!"}
     end
+  end
+
+  def create
+    if current_user.api_key.value == search_params[:api_key]
+      fav = Favorite.create(neo_reference_id: search_params[:neo_reference_id], user_id: current_user.id)
+      render json: fav
+    else
+      render json: {status: "error", code: 403, message: "Trying to use someone else's API key is very naughty. Bad user! Bad!"}
+    end
+  end
+
+private
+
+  def search_params
+    params.require(:action)
+    params.permit(:api_key, :neo_reference_id)
   end
 
 end
--- spec/requests/api/v1/favorites_spec.rb
+++ spec/requests/api/v1/favorites_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
-describe "GET /user/favorites" do
-  it "returns a favorite with a valid API key" do
+describe "As a registered user" do
+  it "a GET /user/favorites returns a favorite with a valid API key" do
     user = create(:user, email: "aunt.jessica@example.com", name: "Jessica Flororata")
     api_key = ApiKey.create(value: "EVEN_MORE_LEGIT", user_id: user.id)
     fav = Favorite.create(neo_reference_id: "2153306", user_id: user.id)
 
     get "/api/v1/user/favorites?api_key=#{api_key.value}"
+
 
     expect(response.status).to eq 200
     data = JSON.parse(response.body, symbolize_names: true)
@@ -15,6 +16,25 @@ describe "GET /user/favorites" do
     expect(data.first[:user_id]).to eq(fav.user_id)
     expect(data.first[:asteroid][:name]).to eq("153306 (2001 JL1)")
     expect(data.first[:asteroid][:is_potentially_hazardous_asteroid]).to eq(false)
+  end
+
+  it "a POST requirest creates a favorite with a valid API key " do
+    user = create(:user, email: "aunt.jessica@example.com", name: "Jessica Flororata")
+    api_key = ApiKey.create(value: "EVEN_MORE_LEGIT", user_id: user.id)
+    neo_ref = '2021277'
+
+    post "/api/v1/user/favorites?neo_reference_id=#{neo_ref}&api_key=#{api_key.value}"
+
+    fav = Favorite.last
+
+    expect(response.status).to eq 200
+    data = JSON.parse(response.body, symbolize_names: true)
+    expect(data[:id]).to eq(fav[:id])
+    expect(data[:neo_reference_id]).to eq(neo_ref)
+    expect(data[:user_id]).to eq(user.id)
+    expect(data[:asteroid][:name]).to eq("21277 (1996 TO5)")
+    expect(data[:asteroid][:is_potentially_hazardous_asteroid]).to eq(false)
+
   end
 
 end
